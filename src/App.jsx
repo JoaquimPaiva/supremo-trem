25a92709cfd9e38a755fc249194790c5081b7ae3
JoaquimPaiva <adalberto.j.s.p@gmail.com>
2021-03-02 15:19:51 +0000

    segunda versao

--- src/App.jsx
+++ src/App.jsx
@@ -10,48 +10,70 @@ import CondicionalComIf from './components/basicos/CondicionalComIf';
 import Condicional from './components/basicos/Condicional';
 import Pai from './components/comunicacao/direta/Pai'
 import Super from './components/comunicacao/indireta/super'
+import Input from './components/Form/Input'
+import Contador from './components/contador/Contador'
+import Mega from './components/mega/Mega'
 
 export default (props) => (
     <div className="App">
-        
-        <Card titulo="#08 - Communicação Indireta" color="#000">
-            <Super></Super>
-        </Card>
 
-        <Card titulo="#07 - Communicação Direta" color="#4298B5">
-            <Pai sobrenome="Freitas"></Pai>
-        </Card>
+        <h1>Fundamentos React</h1>
 
-        <Card titulo="#06 - Condicional v2" color="#FA6900">
-            <CondicionalComIf numero={10}></CondicionalComIf>
-        </Card>
+        <div className="Cards">
+            
+            <Card titulo="#11 - Mega" color="#73503C">
+                <Mega qtdeNumero={8}></Mega>
+            </Card>
 
-        <Card titulo="#05 - Condicional v1" color="#E94C6F">
-            <Condicional numero={11}></Condicional>
-        </Card>
+            <Card titulo="#10 - Contador" color="#293E6A">
+                <Contador passo={10} valor={100}></Contador>
+            </Card>
 
-        <Card titulo="#04 - Repetição" color="#008BBA">
-            <Repeticao></Repeticao>
-        </Card>
+            <Card titulo="#09 - Input" color="#9C0F5F">
+                <Input></Input>
+            </Card>
 
-        <Card titulo="#03 - Componente Com Filhos" color="#D96459">
-            <ComFilhos>
-                <ul>
-                    <li>Ana</li>
-                    <li>Bia</li>
-                    <li>Carlos</li>
-                    <li>Daniel</li>
-                </ul>
-            </ComFilhos>
-        </Card>
+            <Card titulo="#08 - Communicação Indireta" color="#000">
+                <Super></Super>
+            </Card>
 
-        <Card titulo="#02 - Componentes Com Parametro" color="#FF85CB">
-            <ComParametro titulo="Esse é o titulo" subtitulo="Esse é o subtitulo" />
-        </Card>
+            <Card titulo="#07 - Communicação Direta" color="#4298B5">
+                <Pai sobrenome="Freitas"></Pai>
+            </Card>
 
-        <Card titulo="#01 - Primeiro Componente" color="#92B06A">
-            <Primeiro />
-        </Card>
+            <Card titulo="#06 - Condicional v2" color="#FA6900">
+                <CondicionalComIf numero={10}></CondicionalComIf>
+            </Card>
+
+            <Card titulo="#05 - Condicional v1" color="#E94C6F">
+                <Condicional numero={11}></Condicional>
+            </Card>
+
+            <Card titulo="#04 - Repetição" color="#008BBA">
+                <Repeticao></Repeticao>
+            </Card>
+
+            <Card titulo="#03 - Componente Com Filhos" color="#D96459">
+                <ComFilhos>
+                    <ul>
+                        <li>Ana</li>
+                        <li>Bia</li>
+                        <li>Carlos</li>
+                        <li>Daniel</li>
+                    </ul>
+                </ComFilhos>
+            </Card>
+
+            <Card titulo="#02 - Componentes Com Parametro" color="#FF85CB">
+                <ComParametro titulo="Esse é o titulo" subtitulo="Esse é o subtitulo" />
+            </Card>
+
+            <Card titulo="#01 - Primeiro Componente" color="#92B06A">
+                <Primeiro />
+            </Card>
+
+        </div>
+
     </div>
 
 );
---
+++ src/components/mega/Mega.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+export default (props) => {
+    
+    const [numeros, setNumeros] = useState(Array(props.qtdeNumero).fill(0))
+
+    function gerarNumerosNaoContido(array) {
+        const min = 1
+        const max = 60 
+        const novoNumero = parseInt(Math.random() * (60 - 1)) + min
+        return array.includes(novoNumero)
+            ? gerarNumerosNaoContido(array) : novoNumero
+    }
+
+    function gerarNumeros() {
+        const novoArray = Array(props.qtdeNumero)
+            .fill(0)
+            .reduce((a, e) => {
+                const novoNumero =  gerarNumerosNaoContido(a)
+                return [...a, novoNumero]
+            }, [])
+            .sort((a, b) => a - b)
+        setNumeros(novoArray)
+    }
+
+    return (
+        <>
+            <h3>Mega</h3>
+            <h4>{numeros.join(' ')}</h4>
+            <button onClick={gerarNumeros}>Gerar Números</button>
+        </>
+    )
+}
